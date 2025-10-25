5923d22cfe7e410feeeb6e10e19cf937f0af68ba
barebone <ashwinijhadps@gmail.com>
2025-10-26 03:17:18 +0530

    Updated Index.html

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import string
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
